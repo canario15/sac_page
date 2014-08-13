aabd37970ccf1b5e4d9352647942932f538fec7a
German <german@german-HP-G50-Notebook-PC.(none)>
2014-08-13 00:25:58 -0300

    create categories links

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,4 +9,42 @@ class CategoriesController < ApplicationController
   def karting_125
   end
 
+  def championship_formula_sac
+  end
+
+  def championship_turismo_pista_sac
+  end
+
+  def championship_karting_125
+  end
+
+  def pilots_formula_sac
+  end
+
+  def pilots_turismo_pista_sac
+  end
+
+  def pilots_karting_125
+  end
+
+  def gallery_formula_sac
+  end
+
+  def gallery_turismo_pista_sac
+  end
+
+  def gallery_karting_125
+  end
+
+  def regulation_formula_sac
+  end
+
+  def regulation_turismo_pista_sac
+  end
+
+  def regulation_karting_125
+  end
+
+
+
 end
