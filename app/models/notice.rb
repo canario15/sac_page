2d575dec6e29c9461ece53236276def51e416141
canario15 <germanfagalde@gmail.com>
2015-07-21 13:01:17 -0300

    default socpe notices

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -5,4 +5,6 @@ class Notice < ActiveRecord::Base
 	has_attached_file :photo, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "/assets/logo.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
+  default_scope order('created_at DESC')
+
 end
